c2360c2698a20f807e52f6c1b54878a8ef065a1e
bwyborney <bwyborney@gmail.com>
2019-08-30 19:35:18 -0700

    whoops it's broken now
    (☞ﾟヮﾟ)☞

---
+++ guides.js
@@ -0,0 +1,20 @@
+function switchTab(evt, game)
+{
+	var i, tabcontent, tablinks;
+	
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++)
+		{
+			tabcontent[i].style.display = "none";
+		}
+	
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++)
+		{
+			tablinks[i].className = tablinks[i].className.replace(" active", "");
+		}
+	
+	document.getElementByID(game).style.display = "block";
+	evt.currentTarget.className += " active";
+	
+}
