a68c23e6a47ac3a5f66f3836eb8ca06eee8d9e37
bwyborney <bwyborney@gmail.com>
2019-08-30 21:24:46 -0700

    it's broken again
    whoops

--- guides.js
+++ guides.js
@@ -1,3 +1,4 @@
+/*
 function switchTab(evt, game)
 {
 	var i, tabcontent, tablinks;
@@ -18,26 +19,28 @@ function switchTab(evt, game)
 	evt.currentTarget.className += " active";
 	
 }
+*/
 
-/*
-function switchTab(evt, game) {
-  // Declare all variables
+function switchTab(pageName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
   var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].style.backgroundColor = "";
   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(game).style.display = "block";
-  evt.currentTarget.className += " active";
-} 
-*/
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click(); 
